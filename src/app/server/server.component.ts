fafe75800b2b96583e1a0457156f6302bfe4b017
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2021-08-20 17:20:01 -0400

    data binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,10 +3,14 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-server',
     templateUrl: './server.component.html',
-    styleUrls: ['./app.component.css']
+    // styleUrls: ['./app.component.css']
 })
 
 export class ServerComponent {
     serverId: number = 10;
-    serverStatus: string = 'offline'
+    serverStatus: string = 'offline';
+
+    getServerStatus() {
+        return this.serverStatus;
+    }
 }
